46025bb274c2743671ee3628570837ee4c664b86
Jack Cullen <jpcullen20@gmail.com>
2020-04-22 10:04:59 -0600

    Add enough_ingredients_for? method

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -12,4 +12,11 @@ class Pantry
   def restock(ingredient, amount)
     @stock[ingredient] += amount
   end
+
+  def enough_ingredients_for?(recipe)
+    recipe.ingredients_required.all? do |ingredient, amount|
+      stock_check(ingredient) >= amount
+    end
+  end
+  
 end
